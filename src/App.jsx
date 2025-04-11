2f83a3ca2dc75497204fe0f4fb73ccc4b43dd0c4
Anas Ansari <anasansari056789@gmail.com>
2025-04-12 00:43:49 +0530

    Add the Initial Buildup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import Testimonials from "./sections/Testimonials";
+import Footer from "./sections/Footer";
+import Contact from "./sections/Contact";
+import TechStack from "./sections/TechStack";
+import Experience from "./sections/Experience";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import LogoShowcase from "./sections/LogoShowcase";
+import FeatureCards from "./sections/FeatureCards";
+import Navbar from "./components/NavBar";
 
-const App = () => {
-  return (
-    <main>
-      <div>
-        <h1>Welcome to my 3D Portfolio!</h1>
-      </div>
-    </main>
-  );
-};
+const App = () => (
+  <>
+    <Navbar />
+    <Hero />
+    <ShowcaseSection />
+    <LogoShowcase />
+    <FeatureCards />
+    <Experience />
+    <TechStack />
+    <Testimonials />
+    <Contact />
+    <Footer />
+  </>
+);
 
 export default App;
